e29274ce25778094d023949f6bc266ec16a9ce64
PoorBearr <113504306+PoorBearr@users.noreply.github.com>
2022-12-27 16:04:09 +0300

    V.2
    Прописаны все необходимые функции для работы с деревом

--- main.cpp
+++ main.cpp
@@ -17,6 +17,105 @@ struct Node {
 
 struct tree {
     Node *root = nullptr;
+
+    void push(Node *p, Node* cur) {
+        if (root == nullptr) {
+            root = p;
+            return;
+        }
+        while (cur != nullptr)  {
+            if (cur->data < p->data) {
+                if (cur->r != nullptr) cur = cur->r;
+                else {
+                    p->par = cur;
+                    cur->r = p;
+                    break;
+                }
+            } else if (cur->data == p->data) break;
+            else if (cur->data > p->data) {
+                if (cur->l != nullptr) cur = cur->l;
+                else {
+                    p->par = cur;
+                    cur->l = p;
+                }
+            }
+        }
+    }
+
+    Node *exist(Node* cur , int x) {
+        if (cur == nullptr or x == cur->data) return cur;
+        if (x < cur->data) return exist(cur->l, x);
+        else return exist(cur->r, x);
+    }
+
+    Node *next(int x) {
+        Node *cur = root;
+        Node *ans = nullptr;
+        while (cur != nullptr) {
+            if (cur->data > x) {
+                ans = cur;
+                cur = cur->l;
+            } else cur = cur->r;
+        }
+        return ans;
+    }
+
+    Node *prev(int x) {
+        Node *cur = root;
+        Node *ans = nullptr;
+        while (cur != nullptr) {
+            if (cur->data >= x) cur = cur->l;
+            else {
+                ans = cur;
+                cur = cur->r;
+            }
+        }
+        return ans;
+    }
+
+    void del(Node *p, int x) {
+        if (p == nullptr) return;
+        Node *node = p->par;
+        if (p->l == nullptr && p->r == nullptr) {
+            if (node == nullptr) {
+                root = nullptr;
+                return;
+            }
+            if (node->l == p) node->l = nullptr;
+            if (node->r == p) node->r = nullptr;
+        } else
+            if (p->l == nullptr or p->r == nullptr) {
+                if (p->l == nullptr) {
+                    if (node == nullptr) {
+                        root = p->r;
+                        root->par = nullptr;
+                        return;
+                    }
+                    if (node->l == p) node->l = p->r;
+                    else node->r = p->r;
+                    p->r->par = node;
+                } else {
+                    if (node == nullptr) {
+                        root = p->l;
+                        root->par = nullptr;
+                        return;
+                    }
+                    if (node->l == p) node->l = p->l;
+                    else node->r = p->l;
+                    p->l->par = node;
+                }
+            } else {
+            Node *cur = next(x);
+            p->data = cur->data;
+            if (cur->par->l == cur) {
+                cur->par->l = cur->r;
+                if (cur->r != nullptr) cur->r->par = cur->par;
+            } else {
+                cur->par->r = cur->r;
+                if (cur->r != nullptr) cur->r->par = cur->par;
+            }
+        }
+    }
 };
 
 int main() {
